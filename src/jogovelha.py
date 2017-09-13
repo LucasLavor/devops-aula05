869c87f963af5a5d5a66acbca0acbf53b9c36a21
LucasLavor <30813738+LucasLavor@users.noreply.github.com>
2017-09-12 21:56:29 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,9 +1,15 @@
-d q
-	pll
+def inicializar():
+	tab = [ ]
+	for i in range(3):
+	 linha = [ ]
+	 for j in range(3):
+	 	linha.append(".")
+	 tab.append(linha)
+	return tab
+	
 def main( ):
  	jogo = inicializar( )
  	print (jogo)
  	
- if __name__ == "__main__": de12
+ if __name__ == "__main__":
  main()
-dwq dq d e1

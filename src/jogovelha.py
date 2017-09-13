f68d5c05eaa07aee15cd2f72d964b5d42b778f92
LucasLavor <30813738+LucasLavor@users.noreply.github.com>
2017-09-12 22:03:12 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,13 +1,15 @@
-def in123icial21wqe312312izar():
+def inicializar():
 	tab = [ ]
-	fodewqei in range(3):
+	for i in range(3):
 	 linha = [ ]
-	 f31	linhaappend("."32423)23e23
-	 ta123urn t2312ab
-	wdq12e12
+	 for j in range(3):
+	 	linha.append(".")
+	 tab.append(linha)
+	return tab
+	
 def main( ):
  	jogo = inicializar( )
  	print (jogo)
  	
- i213123f 12312__na213123me__ == "__main__":
+ if __name__ == "__main__":
  main()

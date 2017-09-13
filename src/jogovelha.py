6028e363991bea18d31ddc2b44d7904d7de63c42
LucasLavor <30813738+LucasLavor@users.noreply.github.com>
2017-09-12 21:59:08 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,8 +3,8 @@ def inicializar():
 	for i in range(3):
 	 linha = [ ]
 	 for j in range(3):
-	 	linha.append(".")
-	 tab.append(linha)
+	 	linhaappend("."32423)23e23
+	 tab.append(linha)qwdqwd342
 	return tab
 	
 def main( ):
